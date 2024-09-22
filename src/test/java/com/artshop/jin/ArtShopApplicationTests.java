2d2a9c1445721044d814228afe62d9fd60bfcd50
Jin <nancy.jin.aiesecxmu@gmail.com>
2024-09-23 00:58:35 +0900

    4
    4

---
+++ src/test/java/com/artshop/jin/ArtShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artshop.jin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
