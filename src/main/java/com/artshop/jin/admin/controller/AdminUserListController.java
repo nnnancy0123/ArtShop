54a724554657e78b625677d75d81430004646d49
Jin <106419088+nnnancy0123@users.noreply.github.com>
2024-09-30 19:26:45 +0900

    u
    00

--- src/main/java/com/artshop/jin/admin/controller/AdminUserListController.java
+++ src/main/java/com/artshop/jin/admin/controller/AdminUserListController.java
@@ -2,6 +2,8 @@ package com.artshop.jin.admin.controller;
 
 import java.util.List;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,21 @@ import org.springframework.web.servlet.ModelAndView;
 import com.artshop.jin.admin.object.AdminUserListObject;
 import com.artshop.jin.admin.service.AdminUserListService;
 
-import jakarta.persistence.EntityNotFoundException;
+/**
+ * ユーザー管理クラス
+ * @author Nancy
+ * @sinces  2024-09-30
+ */
 
 @RestController
 public class AdminUserListController {
 	@Autowired
 	private AdminUserListService adminUserListService;
 
-	//ユーザー管理リストが画面表示される
+	/**
+	 * ユーザー管理リストが画面表示される
+	 * @return
+	 */
 	@GetMapping("/adminuserlist")
 	public ModelAndView adminUserList() {
 
@@ -34,7 +43,11 @@ public class AdminUserListController {
 		return mav;
 	}
 
-	// ユーザーIDでユーザー情報を取得する
+	/**
+	 * ユーザーIDでユーザー情報を取得する
+	 * @param userId
+	 * @return
+	 */
 	@GetMapping("/getUserInfo")
 	public ResponseEntity<AdminUserListObject> getUserInfo(@RequestParam("userId") Long userId) {
 		AdminUserListObject userInfoById = adminUserListService.getUserInfoById(userId);
@@ -49,28 +62,33 @@ public class AdminUserListController {
 
 	//ユーザーIDでユーザ情報を更新して保存する
 	@PostMapping("/saveUserInfo")
-	public ResponseEntity<AdminUserListObject> saveUserInfoById(@RequestBody AdminUserListObject user) {
+	public ResponseEntity<AdminUserListObject> saveUserInfoById(@RequestBody AdminUserListObject adminUserListObject) {
 
-		AdminUserListObject updateUserInfo = adminUserListService.saveUserinfoById(user.getUsersId(),
-				user.getUsersName(),
-				user.getUsersMail(),
-				user.getUsersRoles(),
-				user.getUsersPoints(),
-				user.getPostCode(),
-				user.getUsersAddress1(),
-				user.getUsersAddress2(),
-				user.getUsersAddress3(),
-				user.getDelFlag());
-		;
+		AdminUserListObject updateUserInfo = adminUserListService.saveUserinfoById(adminUserListObject.getUsersId(),
+				adminUserListObject.getUsersName(),
+				adminUserListObject.getUsersMail(),
+				adminUserListObject.getUsersRoles(),
+				adminUserListObject.getUsersPoints(),
+				adminUserListObject.getPostCode(),
+				adminUserListObject.getUsersAddress1(),
+				adminUserListObject.getUsersAddress2(),
+				adminUserListObject.getUsersAddress3(),
+				adminUserListObject.getDelFlag());
+
 		if (updateUserInfo != null) {
 			//成功する時、HTTPステータスコード200を設定する
 			return ResponseEntity.ok(updateUserInfo);
-		} else {
-			// ユーザが見つからない時、エラーメッセージを返す
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+		// ユーザが見つからない時、エラーメッセージを返す
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
 	}
 
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
 	@PostMapping("/delUserInfo")
 	public ResponseEntity<String> delUserInfo(@RequestParam("userId") Long userId) {
 		try {
